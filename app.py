872d9fa54164a03cb3a92aa39504af7263a17f72
Elpappy3595 <leonaldyryco@gmail.com>
2026-08-20 22:03:49 +0700

    Add vending server

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "VENDING SERVER ONLINE"
+
+
+@app.route("/status")
+def status():
+    return jsonify({
+        "status": "READY",
+        "message": "VENDING SERVER ONLINE"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
